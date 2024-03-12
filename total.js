b92e90bdace16505d8b47058485fb77e51dd61ee
parameth <palm24572@gmail.com>
2024-03-12 20:18:50 +0700

    extect function

--- total.js
+++ total.js
@@ -1,19 +1,20 @@
 function total(numbers) {
-    var result = 0;
-    var parts = numbers.split(',');
-    for (var i = 0; i < parts.length; i++) {
-        var integer = parseInt(parts[i]);
-        //Check if integer
-        if (!isNaN(integer)) {
-            //Check is an integer is in a valid range
-            if (integer >= 0) {
-                if (integer <= 1000) {
-                    result += integer;
-                }
-            }
-        }
-    }
-    return result;
+  return numbers
+    .split(",")
+    .map((part) => parseInt(part))
+    .filter(
+      (integer) =>
+      checkIsInteger(integer) && checkIsAnIntegerIsInAValidRange(integer)
+    )
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+function checkIsInteger(integer) {
+  return !isNaN(integer);
+}
+
+function checkIsAnIntegerIsInAValidRange(integer) {
+  return integer <= 1000 && integer >= 0;
 }
 
 module.exports = total;
